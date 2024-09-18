40366b5f06f8bae769d87d0c35e3c03e3f02ceda
coinvest518 <coinvest518@gmail.com>
2024-09-17 23:22:32 -0400

    Updated user data API handler

--- pages/api/user-data.ts
+++ pages/api/user-data.ts
@@ -2,28 +2,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../lib/prisma';
 import { planDetails } from '../../lib/planDetails';
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { userId } = req.query;
-
-  console.log('Received userId:', userId);
 
   if (!userId) {
     return res.status(400).json({ message: 'User ID is required' });
   }
 
   try {
-    const startTime = Date.now(); // Start timing
     const user = await prisma.user.findUnique({
       where: { clerkUserId: userId as string },
+
     });
-    const duration = Date.now() - startTime; // Calculate duration
-    console.log(`Database query duration: ${duration}ms`);
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
     const plan = planDetails.find(p => p.priceId === user.stripePriceId);
+    
 
     const userData = {
       ...user,
