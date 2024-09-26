bc5c49d01c18715ba4c47467feda7ab7022de612
promise Me <coinvest518@gmail.com>
2024-09-26 10:58:35 -0400

    new user prompt api

--- pages/api/user-prompts.ts
+++ pages/api/user-prompts.ts
@@ -20,17 +20,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(404).json({ message: 'User not found' });
     }
 
-    const userPrompts = await prisma.userPrompt.findMany({
-      where: { clerkUserId: userId as string},
-      select: { id: true },
-
-    });
-
-    
-
-    return res.status(200).json(userPrompts);
+    try {
+      const userPrompts = await prisma.userPrompt.findMany({
+        where: { clerkUserId: userId as string },
+        select: { 
+          id: true,
+          promptTitle: true,
+          imgSrc: true,
+          description: true,
+          creditPrice: true,
+          category: true,
+        },
+      });
+  
+      return res.status(200).json({ userPrompts });
+    } catch (error) {
+      console.error('Error fetching user prompts:', error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
   } catch (error) {
-    console.error('Error fetching user prompts:', error);
+    console.error('Error fetching user data:', error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
 }
+
