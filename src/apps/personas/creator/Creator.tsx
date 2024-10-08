83e407b1917b3569f023b50fa60051b2a631b046
promise Me <coinvest518@gmail.com>
2024-10-07 23:49:50 -0400

    yt

--- src/apps/personas/creator/Creator.tsx
+++ src/apps/personas/creator/Creator.tsx
@@ -16,6 +16,7 @@ import { useLLMSelect, useLLMSelectLocalState } from '~/common/components/forms/
 import { useToggleableBoolean } from '~/common/util/useToggleableBoolean';
 
 import { FromText } from './FromText';
+import { FromYouTube } from './FromYouTube';
 import { prependSimplePersona, SimplePersonaProvenance } from '../store-app-personas';
 
 
@@ -198,10 +199,11 @@ export function Creator(props: { display: boolean }) {
           '& > *:first-of-type': { borderTopLeftRadius: '0.5rem' },
         }}
       >
-       
+        
         <Tab>From Text</Tab>
       </TabList>
       <TabPanel keepMounted value={0} sx={{ p: 3 }}>
+        <FromYouTube isTransforming={isTransforming} onCreate={handleCreate} />
       </TabPanel>
       <TabPanel keepMounted value={1} sx={{ p: 3 }}>
         <FromText isCreating={isTransforming} onCreate={handleCreate} />
